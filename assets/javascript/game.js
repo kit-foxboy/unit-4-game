3cbecdb50e426b5d0795403252a427d13348df10
Geoff Lambert <admin@vulpineinteractive.com>
2018-09-27 16:11:01 -0600

    fixed combat, made html more dynamic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -33,79 +33,144 @@ $(document).ready(function() {
                 new Fighter('Darth Malak', 'assets/images/darth_malak.jpg', 100, 6, 25),
                 new Fighter('Darth Nihilus', 'assets/images/darth_nihilus.png', 200, 4, 10)
             ]
-
             $('#game-area').css('background', 'url(assets/images/kotorGray.png)');
-            $('#title').text('Select a Fighter');
-            $('#subtitle').html('&nbsp; <br> &nbsp;');
-            $('#player-select').show();
-            $('#enemy-select').hide();
-            $('#attacker-area').hide();
-            $('#attacker-area').css('opacity', 1.0);
-            $('#defender-area').hide();
 
             this.update();
         },
 
-        clear: function() {
-            $('#title').text('May the Force be With You');
-            $('#subtitle').text('Click anywhere to play');
-            $('#player-select').hide();
-            $('#enemy-select').hide();
-            $('#attacker-area').hide();
-            $('#defendet-area').hide();
-        },
-
         update: function() {
+            
+            $('.ui').html('');
 
             if(this.currentPlayer && this.currentPlayer.currentHealth <= 0) {
-                
-                $('#attacker-area').animate({opacity: 0}, 500, function() {
-                    game.isActive = false;
-                    $('#subtitle').html('Lethal damage to ' + game.currentPlayer.name + '<br>Click to play again');
-                });
+                this.killPlayer();
                 return;
             }
 
             if(this.currentEnemy && this.currentEnemy.currentHealth <= 0) {
-
+                this.killEnemy();
             }
 
             if (this.currentPlayer === false || this.currentEnemy === false) {
-                
-                var selectDiv = (this.currentPlayer === false) ? '#player-select .character' : '#enemy-select .character';
-                $(selectDiv).each(function(idx) {
-                        
-                    $(this).children('.name').text(game.players[idx].name);
-                    $(this).children('img').attr('src', game.players[idx].imgSrc);
-                    $(this).children('.health').text(game.players[idx].currentHealth);
-                });
+
+                if (this.currentPlayer === false) {
+                    this.showPlayerSelect();
+                } else {
+                    this.showEnemySelect();
+                }
 
             } else {                
-                
-                $('#attacker-area img').attr('src', this.currentPlayer.imgSrc);
-                $('#attacker-area .health').text(this.currentPlayer.currentHealth);
-                $('#defender-area img').attr('src', this.currentEnemy.imgSrc);
-                $('#defender-area .health').text(this.currentEnemy.currentHealth);
+                this.showBattlefield();
             }
+        },
+
+        showPlayerSelect: function() {
+
+            $('#title').text('Select a Fighter');
+
+            for (var i = 0; i < this.players.length; i++) {
+                
+                var character = '\
+                    <div class="character" data-idx="' + i + '">\
+                        <p class="name">' + game.players[i].name + '</p>\
+                            <img src="' + game.players[i].imgSrc + '" width="150">\
+                        <p class="health">' + game.players[i].currentHealth + '</p>\
+                    </div>';
+                $('#player-select').append(character);
+            }
+
+            $('#player-select .character').on('click', function() {
+                if (!game.currentPlayer) {
+                    game.selectPlayer($(this).data('idx'));
+                }
+            });
+        },
+
+        showEnemySelect: function() {
+            
+            $('#title').text('Select an Opponent');
+
+            for (var i = 0; i < this.players.length; i++) {
+                
+                var character = '\
+                    <div class="character" data-idx="' + i + '">\
+                        <p class="name">' + game.players[i].name + '</p>\
+                            <img src="' + game.players[i].imgSrc + '" width="150">\
+                        <p class="health">' + game.players[i].currentHealth + '</p>\
+                    </div>';
+                $('#enemy-select').append(character);
+            }
+
+            $('#enemy-select .character').on('click', function() {
+
+                if (!game.currentEnemy) {
+                    game.selectEnemy($(this).data('idx'));
+                }
+            });
+        },
+
+        showBattlefield: function() {
+
+            $('#title').text('Fight!');
+
+            var attacker = '\
+                <div class="character no-border">\
+                    <p class="name">' + this.currentPlayer.name + '</p>\
+                        <img src="' + this.currentPlayer.imgSrc + '" width="150">\
+                    <p class="health">' + this.currentPlayer.currentHealth + '</p>\
+                    <button type="button" id="attack">Attack</button>\
+                </div>';
+            var defender = '\
+                <div class="character enemy no-border">\
+                    <p class="name">' + this.currentEnemy.name + '</p>\
+                        <img src="' + this.currentEnemy.imgSrc + '" width="150">\
+                    <p class="health">' + this.currentEnemy.currentHealth + '</p>\
+                </div>';
+
+            $('#attacker-area').append(attacker);
+            $('#defender-area').append(defender);
+            $('#attacker-area').show();
+            $('#defender-area').show();
+
+            $('#attack').on('click', function() {
+
+                if (game.currentPlayer && game.currentEnemy) {
+                    game.attack();
+                }
+            });
         },
 
         selectPlayer: function(idx) {
 
             this.currentPlayer = this.players.splice(idx, 1)[0];
-            $('#player-select').hide();
-            $('#enemy-select').show();
-            $('#title').text("Select an Opponent");
             this.update();
         },
 
         selectEnemy: function(idx) {
 
             this.currentEnemy = this.players.splice(idx, 1)[0];
-            $('#enemy-select').hide();
-            $('#attacker-area').show();
-            $('#defender-area').show();
-            $('#title').text("Fight!");
             this.update();
+        },
+
+        killPlayer: function () {
+
+            $('#attacker-area').fadeOut(500, function() {
+                $('#subtitle').html('Lethal damage to ' + game.currentPlayer.name + '<br>Click to play again');
+                game.isActive = false;
+            });
+            $('#defender-area').fadeOut(500);
+        },
+
+        killEnemy: function() {
+
+            $('#attack').attr('disabled', 'disabled');
+            $('#attacker-area').fadeOut(500);
+            $('#defender-area').fadeOut(500, function() {
+                $('#title').text("Select an Opponent");
+                $('#subtitle').html('Lethal damage to ' + game.currentEnemy.name + '<br>&nbsp;');
+                game.currentEnemy = false;
+                game.showEnemySelect();
+            });
         },
 
         attack: function() {
@@ -124,33 +189,11 @@ $(document).ready(function() {
         }
     };
 
-    game.clear();
-
     //define interactions
     $('#game-area').on('click', function() {
 
         if (!game.isActive) {
             game.initGame();
-        }
-    });
-    $('#player-select .character').on('click', function() {
-
-        if (!game.currentPlayer) {
-            game.selectPlayer($(this).data('idx'));
-        }
-    });
-
-    $('#enemy-select .character').on('click', function() {
-
-        if (!game.currentEnemy) {
-            game.selectEnemy($(this).data('idx'));
-        }
-    });
-
-    $('#attack').on('click', function() {
-
-        if (game.currentPlayer && game.currentEnemy) {
-            game.attack();
         }
     });
 });
